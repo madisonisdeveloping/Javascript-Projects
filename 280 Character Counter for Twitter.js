73bc49a6c5ea8cca33b790851c00c8ac79d6f6f7
madisonisdeveloping <129033421+madisonisdeveloping@users.noreply.github.com>
2023-06-19 18:19:58 -0400

    Create 280 Character Counter for Twitter.js

---
+++ 280 Character Counter for Twitter.js
@@ -0,0 +1,7 @@
+// Preview here: https://www.codeply.com/p/N5iDfxs7G0
+
+var message = prompt("Write tweet here:");
+var maxCharacters = 280;
+var charactersLeft = maxCharacters - message.length;
+
+alert("You have written " + message.length + " characters, you have " + charactersLeft+ " characters left.");
