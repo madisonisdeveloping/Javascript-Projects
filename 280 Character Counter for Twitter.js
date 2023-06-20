edf94824a64311360c9ec302c3cb7d9b6ebb45d7
madisonisdeveloping <129033421+madisonisdeveloping@users.noreply.github.com>
2023-06-20 19:39:03 -0400

    Update 280 Character Counter for Twitter.js
    Added a project Goal.

--- 280 Character Counter for Twitter.js
+++ 280 Character Counter for Twitter.js
@@ -1,3 +1,4 @@
+// Goal: Tell a user how many characters their tweet is, as well as how many characters they have before they hit the 280 limit.
 // Preview here: https://www.codeply.com/p/N5iDfxs7G0
 
 var message = prompt("Write tweet here:");
